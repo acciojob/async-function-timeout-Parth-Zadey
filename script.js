dcb5862fa1423cdb18f347b92bd8ef484960fd74
Parth-Zadey <parthzadey35@gmail.com>
2026-09-21 23:25:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const textInput = document.getElementById("text");
+const delayInput = document.getElementById("delay");
+const btn = document.getElementById("btn");
+const output = document.getElementById("output");
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+btn.addEventListener("click", async () => {
+  const message = textInput.value;
+  const time = Number(delayInput.value);
+
+  await delay(time);
+
+  output.innerText = message;
+});
